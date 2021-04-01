bcdce011c2b651b3dfec84124fe9c8eb86fdfdaf
Will Rollitt <72352225+willrollitt@users.noreply.github.com>
2021-04-01 18:18:23 +0100

    Update config.h

--- surf-2.0/config.h
+++ surf-2.0/config.h
@@ -165,5 +165,5 @@ static Button buttons[] = {
 	{ OnMedia,      MODKEY,         1,      clickexternplayer, { 0 },       1 },
 };
 
-#define HOMEPAGE "https://duckduckgo.com/"
-//#define HOMEPAGE "https://searx.me/"
+//#define HOMEPAGE "https://duckduckgo.com/"
+#define HOMEPAGE "https://searx.me/"
